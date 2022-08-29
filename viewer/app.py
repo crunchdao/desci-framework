b0ffd060d195d9d5b461abb2d8d1a699ac8b0b76
Caceresenzo <caceresenzo1502@gmail.com>
2022-08-29 03:00:08 +0200

    fix(viewer): reformatted code

--- viewer/app.py
+++ viewer/app.py
@@ -27,14 +27,16 @@ def validate_signature(response: flask.Request, secret):
     """
 
     # Get the signature from the payload
-    signature_header = response.headers['X-Hub-Signature']
-    sha_name, github_signature = signature_header.split('=')
-    if sha_name != 'sha1':
-        print('ERROR: X-Hub-Signature in payload headers was not sha1=****')
+    signature_header = response.headers["X-Hub-Signature"]
+    sha_name, github_signature = signature_header.split("=")
+    if sha_name != "sha1":
+        print("ERROR: X-Hub-Signature in payload headers was not sha1=****")
         return False
-      
+
     # Create our own signature
-    local_signature = hmac.new(secret.encode('utf-8'), msg=response.data, digestmod=hashlib.sha1)
+    local_signature = hmac.new(
+        secret.encode("utf-8"), msg=response.data, digestmod=hashlib.sha1
+    )
 
     # See if they match
     return hmac.compare_digest(local_signature.hexdigest(), github_signature)
@@ -47,12 +49,16 @@ def assets(path):
 
 @app.route(f"/{PAPER_PDF_FILE}")
 def paper():
-    return flask.send_file(f"{get_commit_directory(LATEST_DIRECTORY_NAME)}/{PAPER_PDF_FILE}")
+    return flask.send_file(
+        f"{get_commit_directory(LATEST_DIRECTORY_NAME)}/{PAPER_PDF_FILE}"
+    )
 
 
 @app.route(f"/{INFO_JSON_FILE}")
 def info():
-    return flask.send_file(f"{get_commit_directory(LATEST_DIRECTORY_NAME)}/{INFO_JSON_FILE}")
+    return flask.send_file(
+        f"{get_commit_directory(LATEST_DIRECTORY_NAME)}/{INFO_JSON_FILE}"
+    )
 
 
 @app.route("/")
@@ -60,9 +66,9 @@ def index():
     return flask.send_file("app.html")
 
 
-@app.route("/webhook", methods=['POST'])
+@app.route("/webhook", methods=["POST"])
 def webhook():
-    validate_signature(flask.request, os.environ['GITHUB_WEBHOOK_SECRET'])
+    validate_signature(flask.request, os.environ["GITHUB_WEBHOOK_SECRET"])
     payload = flask.request.json
 
     workflow = payload.get("workflow_run")
@@ -80,9 +86,14 @@ def webhook():
         return "paper artifact not found"
 
     archive_download_url = artefact["archive_download_url"]
-    zip = zipfile.ZipFile(io.BytesIO(requests.get(archive_download_url, headers={
-        "Authorization": f"Token {os.environ['GITHUB_TOKEN']}"
-    }).content))
+    zip = zipfile.ZipFile(
+        io.BytesIO(
+            requests.get(
+                archive_download_url,
+                headers={"Authorization": f"Token {os.environ['GITHUB_TOKEN']}"},
+            ).content
+        )
+    )
 
     if PAPER_PDF_FILE not in zip.namelist():
         return "paper.pdf not in the zip"
@@ -97,7 +108,7 @@ def webhook():
     latest_symlink = get_commit_directory(LATEST_DIRECTORY_NAME)
     if os.path.exists(latest_symlink):
         os.remove(latest_symlink)
-    
+
     os.symlink(commit_id, LATEST_DIRECTORY_NAME, target_is_directory=True)
     os.rename(LATEST_DIRECTORY_NAME, latest_symlink)
 
